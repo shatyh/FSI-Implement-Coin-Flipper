8e398193e055ad69bf42602cf8262f7ffc954940
Tita Hernandez <tita@Titas-MacBook-Pro.local>
2022-06-17 14:42:50 -0700

    assignment changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,70 @@
 // TODO: Declare any global variables we need
+let headCount = 0
+let tailCount = 0
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+    document.getElementById ("flip").addEventListener("click", function() {
+        console.log("click flip");
+     })
+
+    document.getElementById ("clear").addEventListener("click",function() {
+        console.log("reset flip");
+    })
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
+        const flipResult = Math.random();
+
+        let heads = false;
+        if (flipResult < 0.5) {
+            heads = true
+        }
+
+    
+
         // TODO: Update image and status message in the DOM
+        if (heads) {
+            document.querySelector("#penny_img").src =
+             "assets/images/penny-heads.jpg";
+            document.querySelector(".message-container").innerText = "Heads!";
+            headCount++;
+            document.querySelector("#heads").innerText = headCount;
+        } else {
+            document.querySelector("#penny_img").src = 
+             "assets/images/penny-tails.jpg";
+            document.querySelector(".message-container").innerText = "Tails!";
+            tailCount++;
+            document.querySelector("#tails").innerText = tailCount;
+        }
+
+
 
         // Update the scorboard
         // TODO: Calculate the total number of rolls/flips
         // Make variables to track the percentages of heads and tails
+        let total = headCount + tailCount;
+        let percentHead = Math.round((headCount / total) * 100);
+        let percentTail = Math.round((tailCount / total) * 100);
+
+        document.querySelector("#heads-percent").innerText = `${percentHead}%`;
+        document.querySelector("#tails-percent").innerText = `${percentTail}%`;
+
+    
+
+
+
+        headCount = 0
+        tailCount = 0
+        document.querySelector("#status_message").innerText = "New Game!"; 
+        document.querySelector("#tails").innerText = 0;
+        document.querySelector("#heads").innerText = 0;
+        document.querySelector("#heads-percent").innerText = `0%`;
+        document.querySelector("#tails-percent").innerText = `0%`;
+
+    
+   
         // TODO: Use the calculated total to calculate the percentages
         // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
         // TODO: Update the display of each table cell
@@ -24,4 +74,3 @@ document.addEventListener('DOMContentLoaded', function () {
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
-})
